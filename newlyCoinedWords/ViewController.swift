872e03677ea5f70d0772a209732508cf695bc370
paninology <paninology@gmail.com>
2022-07-12 20:33:54 +0900

    Add enums

--- newlyCoinedWords/ViewController.swift
+++ newlyCoinedWords/ViewController.swift
@@ -15,9 +15,20 @@ import UIKit
 //    }
 //}
 
+enum NewWord: String {
+    case word0 = "삼귀자"
+    case word1 = "꾸안꾸"
+    case word2 = "캡이다"
+    case word3 = "방가방가"
+    case word4 = "헐"
+    case word5 = "버카충"
+    
+    
+}
+
 class ViewController: UIViewController {
     
-    let newWords = ["삼귀자": "연애를 시작하기 전 썸 상태 ❤︎", "꾸안꾸": "꾸준히 안하면 꾸물거리게 된다.", "캡이다": "엄청 대단하다~~", "방가방가": "매우 반갑다는 말", "헐": " 매우 놀란 기분을 표현하는 말", "버카충": "버스카드 충전의 약어"]
+    let newWords = [NewWord.word0.rawValue: "연애를 시작하기 전 썸 상태 ❤︎", NewWord.word1.rawValue: "꾸준히 안하면 꾸물거리게 된다.", "캡이다": "엄청 대단하다~~", "방가방가": "매우 반갑다는 말", "헐": " 매우 놀란 기분을 표현하는 말", "버카충": "버스카드 충전의 약어"]
 
     @IBOutlet weak var searchTextField: UITextField!
     @IBOutlet weak var searchButton: UIButton!
@@ -36,7 +47,9 @@ class ViewController: UIViewController {
         
         searchTextField.layer.borderWidth = 2
         buttonAppearanceSetting(hashTag1, cornerRadius: 8, borderWidth: 1.5)
-        buttonAppearanceSetting(hashTag2, cornerRadius: 8, borderWidth: 1)
+        buttonAppearanceSetting(hashTag2, cornerRadius: 8, borderWidth: 1.3)
+        buttonAppearanceSetting(hashTag3, cornerRadius: 8, borderWidth: 1.1)
+        buttonAppearanceSetting(hashTag4, cornerRadius: 8, borderWidth: 1)
     }
 
 
@@ -50,9 +63,7 @@ class ViewController: UIViewController {
 
     @IBAction func searchTextFieldAction(_ sender: UITextField) {
         resignFirstResponder()
-        print("dddd")
         searchAction()
-        
         
     }
     
@@ -76,10 +87,14 @@ class ViewController: UIViewController {
         
     }
     
+    
+    
     func hashTagSuggest() { //연습삼아 했음..지울것
-        var newWordsForHash = newWords
-        hashTag1.setTitle(newWordsForHash.remove(at: newWordsForHash.startIndex).key, for: .normal)
-        hashTag2.setTitle(newWordsForHash.remove(at: newWordsForHash.startIndex).key, for: .normal)
+  
+        hashTag1.setTitle(NewWord.word1.rawValue, for: .normal)
+        hashTag2.setTitle(NewWord.word2.rawValue, for: .normal)
+        hashTag3.setTitle(NewWord.word3.rawValue, for: .normal)
+        hashTag4.setTitle(NewWord.word4.rawValue, for: .normal)
     }
     
 }
